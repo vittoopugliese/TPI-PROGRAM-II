ea322a4c450d8170f74924ba06e97002a6a4d438
Steve <theRealSteveGates@outlook.com>
2024-10-26 11:15:44 -0300

    Agrego clase Movimiento

---
+++ movimiento.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+#include "movimiento.h"
+#include "funcionesGlobales.h"
+
+Movimiento::Movimiento(int idMovimiento, bool idTipo, int idUsuario, int idCategoria, Fecha fecha, float importe, bool recurrencia, const char *descripcion, bool estado){
+    _idMovimiento = idMovimiento;
+    _idTipo = idTipo;  // 0-Credito | 1-Debito
+    _idUsuario = idUsuario;
+    _idCategoria = idCategoria;
+    _fecha = fecha;
+    _importe = importe;
+    _recurrencia = recurrencia;
+    strcpy(_descripcion, descripcion);
+    _estado = estado;
+};
+Movimiento::Movimiento(){
+    _idMovimiento = 0;
+    _idTipo = 0;  // 0-Credito | 1-Debito
+    _idUsuario = 1;
+    _idCategoria = 0;
+    _fecha = Fecha();
+    _importe = 0;
+    _recurrencia = 0;
+    strcpy(_descripcion, "S/D"); // SD = Sin Descripcion
+    _estado = true;
+};
+
+//SETS
+void Movimiento::setIdMovimiento(int idMovimiento){_idMovimiento = idMovimiento;}
+void Movimiento::setIdTipo(bool tipo){_idTipo = idTipo;}
+void Movimiento::setIdUsuario(int idUsuario){_idUsuarip = idUsuario;}
+void Movimiento::setIdCategoria(int idCategoria){_idCategoria = idCategoria;}
+void Movimiento::setFecha(Fecha fecha){_fecha = fecha;}
+void Movimiento::setImporte(float importe){_importe = importe;}
+void Movimiento::setRecurrencia(bool recurrencia){_recurrencia = recurrencia;}
+void Movimiento::setDescripcion(const char *descripcion){strcpy(_descripcion, descripcion);}
+void Movimiento::setEstado(bool estado){_estado = estado;}
+
+//GETS
+int Movimiento::getIdMovimiento(){return _idMovimiento;}
+bool Movimiento::getIdTipo(){return _idTipo;}
+int Movimiento::getIdUsuario(){return _idUsuario;}
+int Movimiento::getIdCategoria(){return _idCategoria;}
+Fecha Movimiento::getFecha(){return _fecha;}
+float Movimiento::getImporte(){return _importe;}
+bool Movimiento::getRecurrencia(){return _recurrencia;}
+const char* Movimiento::getDescripcion(){return _descripcion;}
+bool Movimiento::getEstado(){return _estado;}
---
+++ movimiento.h
@@ -0,0 +1,43 @@
+#pragma once
+using namespace std;
+#include "fecha.h"
+
+ class Movimiento
+ {
+   private:
+    int _idMovimiento;
+    bool _idTipo; // 0-Credito | 1-Debito
+    int _idUsuario;
+    int _idCategoria;
+    Fecha _fecha;
+    float _importe;
+    bool _recurrencia;
+    char _descripcion[100];
+    bool _estado;
+
+   public:
+    Movimiento(int idMovimiento, bool idTipo, int idUsuario, int idCategoria, Fecha fecha, float importe, bool recurrencia, const char *descripcion, bool estado);
+    Movimiento();
+
+    //SETS
+    void setIdMovimiento(int numero);
+    void setIdTipo(bool tipo);
+    void setIdUsuario(int idUsuario);
+    void setIdCategoria(int idCategoria);
+    void setFecha(Fecha fecha);
+    void setImporte(float importe);
+    void setRecurrencia(bool recurrencia);
+    void setDescripcion(const char *descripcion);
+    void setEstado(bool estado);
+
+    //GETS
+    int getIdMovimiento();
+    bool getIdTipo();
+    int getIdUsuario();
+    int getIdCategoria();
+    Fecha getFecha();
+    float getImporte();
+    bool getRecurrencia();
+    const char getDescripcion();
+    bool getEstado();
+ };
