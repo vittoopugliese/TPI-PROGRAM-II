1ba34c7d829505da61ca70511be661957c7db618
Salchy <leandrocg524@gmail.com>
2024-11-14 12:30:55 -0300

    Fix de muestreo de números grandes con tipo float

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <iomanip>
 #include "usuarioManager.h"
 using namespace std;
 
 #include "app.h"
 
 int main() {
+    std::cout << std::fixed;
+    std::cout << std::setprecision(2);
     App menu;
     menu.menuLogin();
     return 0;
